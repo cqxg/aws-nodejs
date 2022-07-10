b494317301c9c6bd317b636bf957b83009d61d16
cqxg <cqxg@yandex.ru>
2022-07-10 22:43:26 +0200

    feat: add catalogBatchProcess update

--- product-service/catalogBatchProcess.js
+++ product-service/catalogBatchProcess.js
@@ -1,7 +1,8 @@
 import AWS from "aws-sdk";
 
-export const catalogBatchProcess = (event, context) => {
+export const catalogBatchProcess = (event, context, callback) => {
   const lambda = new AWS.Lambda({ region: "eu-west-1" });
+  const sns = new AWS.SNS({ region: "eu-west-1" });
 
   event.Records.forEach((item) => {
     lambda.invoke(
@@ -9,14 +10,32 @@ export const catalogBatchProcess = (event, context) => {
         FunctionName: "product-service-dev-postProduct",
         Payload: JSON.stringify(item),
       },
-      function (error, data) {
+      async (error, data) => {
         if (error) {
           context.done("lambda.invoke error", error);
         }
         if (data.Payload) {
+          await sns
+            .publish({
+              Subject: "Product has been published",
+              Message: JSON.stringify(item.body),
+              TopicArn: process.env.SNS_ARN,
+            })
+            .promise()
+            .then(() => {
+              console.log(
+                "Send email for: ",
+                JSON.stringify(JSON.parse(item.body))
+              );
+            })
+            .catch((error) => {
+              console.log("We've got some error: ", error);
+            });
           context.succeed(data.Payload);
         }
       }
     );
+
+    console.log("product posted");
   });
 };
